8270e840d5c6a979d3119b4de625c7cc7081f085
Justin Swanson <justin.c.swanson@gmail.com>
2020-11-29 17:09:46 -0600

    Added wpf proj

---
+++ WPFLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPFLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
